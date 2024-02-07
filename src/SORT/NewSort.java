f6366ab3edd1cf90eca03ac1290aa485f7ff2650
anushka14023 <anushkarai1423@gmail.com>
2024-02-07 09:47:16 +0530

    2nd Commit

--- src/SORT/NewSort.java
+++ src/SORT/NewSort.java
@@ -1,6 +1,5 @@
 package SORT;
-
-public class BubbleSort {
+public class NewSort {
 	 static void bubbleSort(int[] arr) {  
 	        int n = arr.length;  
 	        int temp = 0;  
@@ -18,21 +17,21 @@ public class BubbleSort {
 	  
 	    }  
 	 public static void main(String[] args) {  
-        int arr[] ={3,60,35,2,45,320,5};  
-//        array
+       int arr[] ={3,60,35,2,45,320,5};  
+//       array
+        
+       System.out.println("Array Before Bubble Sort");  
+       for(int i=0; i < arr.length; i++){  
+               System.out.print(arr[i] + " ");  
+       }  
+       System.out.println();  
          
-        System.out.println("Array Before Bubble Sort");  
-        for(int i=0; i < arr.length; i++){  
-                System.out.print(arr[i] + " ");  
-        }  
-        System.out.println();  
-          
-        bubbleSort(arr);//sorting array elements using bubble sort  
-         
-        System.out.println("Array After Bubble Sort");  
-        for(int i=0; i < arr.length; i++){  
-                System.out.print(arr[i] + " ");  
-        }  
+       bubbleSort(arr);//sorting array elements using bubble sort  
+        
+       System.out.println("Array After Bubble Sort");  
+       for(int i=0; i < arr.length; i++){  
+               System.out.print(arr[i] + " ");  
+       }  
 
 }  
 }    
